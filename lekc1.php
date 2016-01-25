109ef9de7c64f23d4c5eee80bb942c64aa052503
venislavvr <venislav.valchev@gmail.com>
2016-01-25 14:53:13 +0200

    php
    php

---
+++ lekc1.php
@@ -0,0 +1,30 @@
+<?php
+//first values
+/*$user="dimitar";
+$role="student";
+$course="Chemistry";
+$duration=14;
+$text="Hi, $user! Y`ve been approved to take part in the $course
+as a $role. the $course will last for $duration days.";
+echo $text;
+
+
+
+
+
+$a=15;
+$s=$a*$a;
+echo "Area is" .$s. "cm2";*/
+
+$a=10;
+$b=15;
+$c=25;
+
+$ress=($a+$b)/($c*2);
+echo $ress ."-";
+
+$res1=($a+$b)/$c*2;
+
+echo $res1;
+
+
